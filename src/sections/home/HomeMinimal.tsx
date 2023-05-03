b752bb5f76377c3d067d8384fd4ec75f9dc15303
AhmadSamandar <124392065+AhmadSamandar@users.noreply.github.com>
2023-05-03 14:51:59 -0400

    Cards-clickable-on-main-page

--- src/sections/home/HomeMinimal.tsx
+++ src/sections/home/HomeMinimal.tsx
@@ -1,7 +1,7 @@
 import { m } from 'framer-motion';
 // @mui
 import { alpha, styled } from '@mui/material/styles';
-import { Box, Card, Container, Typography, Stack } from '@mui/material';
+import { Box, Card, Container, Typography, Stack, ButtonBase } from '@mui/material';
 // components
 import Image from '../../components/image';
 import { MotionViewport, varFade } from '../../components/animate';
@@ -39,6 +39,16 @@ const StyledCard = styled(Card)(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
   textAlign: 'center',
   padding: theme.spacing(10, 5),
+  transition:
+    'transform 0.5s cubic-bezier(0.25, 0.8, 0.25, 1), boxShadow 0.5s cubic-bezier(0.25, 0.8, 0.25, 1)',
+  '&:hover': {
+    transform: 'scale(1.05)',
+    boxShadow: `0 8px 16px ${
+      theme.palette.mode === 'light'
+        ? alpha(theme.palette.grey[500], 0.24)
+        : alpha(theme.palette.common.black, 0.24)
+    }`,
+  },
   [theme.breakpoints.up('md')]: {
     boxShadow: 'none',
   },
@@ -81,31 +91,33 @@ export default function HomeMinimal() {
         >
           {CARDS.map((card, index) => (
             <m.div variants={varFade().inUp} key={card.title}>
-              <StyledCard
-                sx={{
-                  ...(index === 1 && {
-                    boxShadow: (theme) => ({
-                      md: `-40px 40px 80px ${
-                        theme.palette.mode === 'light'
-                          ? alpha(theme.palette.grey[500], 0.16)
-                          : alpha(theme.palette.common.black, 0.4)
-                      }`,
+              <ButtonBase onClick={() => console.log(`Clicked ${card.title}`)}>
+                <StyledCard
+                  sx={{
+                    ...(index === 1 && {
+                      boxShadow: (theme) => ({
+                        md: `-40px 40px 80px ${
+                          theme.palette.mode === 'light'
+                            ? alpha(theme.palette.grey[500], 0.16)
+                            : alpha(theme.palette.common.black, 0.4)
+                        }`,
+                      }),
                     }),
-                  }),
-                }}
-              >
-                <Image
-                  src={card.icon}
-                  alt={card.title}
-                  sx={{ mx: 'auto', width: 48, height: 48 }}
-                />
+                  }}
+                >
+                  <Image
+                    src={card.icon}
+                    alt={card.title}
+                    sx={{ mx: 'auto', width: 48, height: 48 }}
+                  />
 
-                <Typography variant="h5" sx={{ mt: 8, mb: 2 }}>
-                  {card.title}
-                </Typography>
+                  <Typography variant="h5" sx={{ mt: 8, mb: 2 }}>
+                    {card.title}
+                  </Typography>
 
-                <Typography sx={{ color: 'text.secondary' }}>{card.description}</Typography>
-              </StyledCard>
+                  <Typography sx={{ color: 'text.secondary' }}>{card.description}</Typography>
+                </StyledCard>
+              </ButtonBase>
             </m.div>
           ))}
         </Box>
